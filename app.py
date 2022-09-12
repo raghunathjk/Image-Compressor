abefabe99b249a88c88aaeab6dc99dbf4da1fb24
raghunathjk <raghunath.jayakashinat@gmail.com>
2022-09-12 20:01:30 +0530

    comp w/o specs

---
+++ app.py
@@ -0,0 +1,16 @@
+import PIL
+from PIL import Image, ImageFont
+
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import *
+
+
+
+file_path = askopenfilename()
+img = PIL.Image.open(file_path)
+myheight, mywidth = img.size
+format = img.format
+img = img.resize((myheight,mywidth),PIL.Image.ANTIALIAS)
+save_path = asksaveasfilename()
+img.save(save_path+ '_compressed.JPG')
